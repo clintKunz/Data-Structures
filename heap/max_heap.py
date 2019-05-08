a9e51f31e46503a8a91fdeae5bfde02e13e272c2
Clint <clintkunz@gmail.com>
2019-05-08 15:57:45 -0600

    heap tests pass

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,16 +3,30 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+      self.storage.append(value)
+      self._bubble_up(len(self.storage)-1)
+
 
   def delete(self):
-    pass
+    if len(self.storage) == 0:
+      return None
+    elif len(self.storage) == 1:
+      return self.storage.pop()
+    else:
+      deleted = self.storage[0]
+      self.storage[0] = self.storage[len(self.storage)-1]
+      self.storage.pop()
+      self._sift_down(0)
+      return deleted
 
   def get_max(self):
-    pass
+    if self.storage[0] == None:
+      return None
+    else: 
+      return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
     # loop until either the element reaches the top of the array
@@ -31,4 +45,29 @@ class Heap:
         break
 
   def _sift_down(self, index):
-    pass
+    left = (2 * index) + 1
+    right = (2 * index) + 2
+    max_child = index
+    if len(self.storage) > left and self.storage[max_child] < self.storage[left]:
+      max_child = left
+    if len(self.storage) > right and self.storage[max_child] < self.storage[right]:
+      max_child = right
+    if max_child == index:
+      return
+    else:
+      temp = self.storage[index]
+      self.storage[index] = self.storage[max_child]
+      self.storage[max_child] = temp
+      self._sift_down(max_child)
+
+a = Heap()
+a.insert(6)
+a.insert(8)
+a.insert(10)
+a.insert(9)
+a.insert(1)
+a.insert(9)
+a.insert(9)
+a.insert(5) 
+a.delete()
+print(a.storage)
