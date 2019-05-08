8aeea9c5f99412051d6cbd4fef9885b8488bf02d
Clint Kunz <clintkunz@gmail.com>
2019-05-08 10:55:02 -0600

    bst insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,24 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # check if the new node's value is less than our current node's value
+    if value < self.value:
+      # check if no left child
+      if not self.left:
+        # park the new node here
+        self.left = BinarySearchTree(value)
+      # otherwise keep traversing further down
+      else: 
+        self.left.insert(value)
+    #check the right side
+    else:
+      # check if no right child
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else: 
+        # keep recursing down to the right since there is a right child
+        self.right.insert(value)
+
 
   def contains(self, target):
     pass
